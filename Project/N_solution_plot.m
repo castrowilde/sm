5239c83ab661049b180512ffc5791ae3cdf4dd90
PlamenNikiforov <plamen.nikiforow@gmail.com>
2017-05-04 18:18:40 +0300

    some code
    >generated spherical coords, converted to cartesian
    (will upload restriction calculations in the future)
    >Monte Carlo solution
    >plotting solution convergence(depending on coords generated)

---
+++ Project/N_solution_plot.m
@@ -0,0 +1,9 @@
+solutions = zeros(1,10);
+N = zeros(1,10);
+for i = 100:100:1000
+    [solution,n] = Monte_Carlo_solution(i);
+    solutions(i/100) = solution;
+    N(i/100) = n;
+end
+
+plot(N,solutions);
