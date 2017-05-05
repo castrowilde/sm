a097ff9970b0e8ec5a2a10b754671ef0dc8045a6
PlamenNikiforov <plamen.nikiforow@gmail.com>
2017-05-05 14:03:01 +0300

    error plotting

---
+++ Project/error_N_plot.m
@@ -0,0 +1,9 @@
+err = zeros(1,10);
+N = zeros(1,10);
+for i = 100:100:1000
+    [solution,n] = Monte_Carlo_solution(i);
+    err(i/100) = abs((pi/10) - solution);
+    N(i/100) = n;
+end
+
+plot(N,err);
