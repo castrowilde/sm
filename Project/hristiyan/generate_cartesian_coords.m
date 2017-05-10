383c66f4e69adffddebc75eebfe2c78af70d7ebf
Hristiyan Markov <itzomarkov@gmail.com>
2017-05-10 13:01:23 +0300

    Add files via upload

---
+++ Project/hristiyan/generate_cartesian_coords.m
@@ -0,0 +1,11 @@
+function [ x,y,z ] = generate_cartesian_coords( ro,theta,phi )
+    n = length(ro);
+    x = zeros(1,n);
+    y = zeros(1,n);
+    z = zeros(1,n);
+    for i = 1:n
+        x(i) = ro(i)*sin(phi(i))*cos(theta(i));
+        y(i) = ro(i)*sin(phi(i))*sin(theta(i));
+        z(i) = ro(i)*cos(phi(i));
+    end
+end
